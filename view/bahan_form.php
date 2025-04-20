066703aba5c818ab4d5b2a987bf9171fc45da808
Ahmad Izzuddin Azzam <diegomortar@gmail.com>
2025-04-20 22:48:22 +0700

    Delete view directory

--- view/bahan_form.php
+++
@@ -1,6 +0,0 @@
-<form method="POST" action="">
-    <label for="nama_bahan">Nama Bahan:</label>
-    <input type="text" id="nama_bahan" name="nama_bahan" required>
-    
-    <button type="submit" name="add_bahan">Tambah Bahan</button>
-</form>
--- view/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <p>&copy; 2025 Resep Makanan Online</p>
-    <a href="index.php" class="btn-back-to-home">Kembali ke Halaman Utama</a>
-</footer>
